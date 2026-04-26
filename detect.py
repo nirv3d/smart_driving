81375d4b09adb67565aae7f77fb3bf9cf972d2ca
nirv3d <nirved@nirved.com>
2026-04-26 20:58:31 +0530

    Phase 1 done

--- detect.py
+++ detect.py
@@ -29,7 +29,12 @@ while cap.isOpened():#while camera is open
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.putText(frame, f"{label} {conf:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        
+    cv2.imshow("Smart Driving", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'): #0xFF is a bitwise operation to get the last 8 bits of the key pressed, ord('q') gets the ASCII value of 'q'
+        break
+
+cap.release() 
+cv2.destroyAllWindows() 
 
 
 
