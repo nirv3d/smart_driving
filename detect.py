1a8f8f06c93cde201c833c111a08825231b35c45
nirv3d <nirved@nirved.com>
2026-04-27 20:51:38 +0530

    phase 1 competed

--- detect.py
+++ detect.py
@@ -17,14 +17,14 @@ while cap.isOpened():#while camera is open
     #run the model on the frame, verbose = False to disable printing results to console, [0] to get the first result (since model can return multiple results for batch processing, we dont need that here since we are processing one frame at a time) 
     results = model(frame, verbose=False)[0] 
 
-    for box in results.boxes:
-        cls_id = int(box.cls[0]) #get the class id of the detected object, [0] to get the first class id since we are processing one box at a time
+    for box in results.boxes: #box is variable, and results.boxes predefined in YOLO model to get the bounding boxes of detected objects
+        cls_id = int(box.cls[0]) #get the class id of the detected object, [0] to get the first extract from tensor
         if cls_id not in TARGET_CLASSES: #if the class id is not in our target classes, skip it
             continue 
 
         label = TARGET_CLASSES[cls_id] #get the label of the detected object using the class id
-        conf = float(box.conf[0]) #get the confidence of the detected object, [0] to get the first confidence since we are processing one box at a time
-        x1, y1, x2, y2 = map(int, box.xyxy[0]) #get the coordinates of the bounding box, [0] to get the first box since we are processing one box at a time
+        conf = float(box.conf[0]) #get the confidence of the detected object, [0] to extract the first confidence from tensor 
+        x1, y1, x2, y2 = map(int, box.xyxy[0]) #get the coordinates of the bounding box, [0] to extract the first box coordinates from tensor
 
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.putText(frame, f"{label} {conf:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
