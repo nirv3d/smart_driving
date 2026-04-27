a1d0f7ea4ded4a6ec163ed9cbbb09896c9093b37
nirv3d <nirved@nirved.com>
2026-04-27 21:55:15 +0530

    detection list added

--- detectv2.py
+++ detectv2.py
@@ -8,6 +8,26 @@ TARGET_CLASSES = {0: "person", 2: "car", 3: "motorcycle", 9: "traffic light"}
 
 PRIORITY = {0: 1, 2: 2, 3: 2, 9: 3} #priority of detected objects, lower number means higher priority, person has highest priority, traffic light has lowest priority, cae and motorcycle have same priority
 
+def estimate_distance(box_height, frame_height):
+    ratio = box_height / frame_height
+    if ratio > 0.5:
+        return "VERY CLOSE"
+    elif ratio > 0.3:
+        return "CLOSE"
+    elif ratio > 0.1:
+        return "FAR"
+    else:
+        return "VERY FAR"
+    
+def get_priority_label(cls_id):
+    p = PRIORITY[cls_id]
+    if p == 1:
+        return "HIGH PRIORITY"
+    elif p == 2:
+        return "MEDIUM PRIORITY"
+    else:
+        return "LOW PRIORITY"
+
 #initializing webcam, 0 = first webcam
 cap = cv2.VideoCapture(0)
 
@@ -16,13 +36,16 @@ while cap.isOpened():#while camera is open
     if not ret: #if there is no frame,
         break
 
+    frame_height = frame.shape[0] #get the height of the frame to estimate distance of detected objects
     #run the model on the frame, verbose = False to disable printing results to console, [0] to get the first result (since model can return multiple results for batch processing, we dont need that here since we are processing one frame at a time) 
     results = model(frame, verbose=False)[0] 
 
+    detections = [] #list to store detected objects and their priorities
     for box in results.boxes: #box is variable, and results.boxes predefined in YOLO model to get the bounding boxes of detected objects
         cls_id = int(box.cls[0]) #get the class id of the detected object, [0] to get the first extract from tensor
         if cls_id not in TARGET_CLASSES: #if the class id is not in our target classes, skip it
-            continue 
+            continue
+        detections.append((PRIORITY[cls_id], box, cls_id))#append/add the priority and the box and id to the detections list
 
         label = TARGET_CLASSES[cls_id] #get the label of the detected object using the class id
         conf = float(box.conf[0]) #get the confidence of the detected object, [0] to extract the first confidence from tensor 
